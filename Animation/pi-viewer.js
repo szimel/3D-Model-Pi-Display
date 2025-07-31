e7b4db230ef3c01f9ab9cb9e065ce7a72dc66000
Samuel Zimel <samuel.c.zimel@gmail.com>
2025-07-31 14:24:52 -0500

    Most recent (broken) version for chat?

--- Animation/pi-viewer.js
+++ Animation/pi-viewer.js
@@ -50,7 +50,7 @@ async function loadAssets() {
 		const [json, chairGLB, brushGLB] = await Promise.all([
 			fetch('./path-data.json').then(r=>r.json()),
 			loader.loadAsync('../../Models/stool.glb'),
-			loader.loadAsync('../../Models/brush.glb')
+			loader.loadAsync('../../Models/brush_2.glb')
 		]);
 
 		AnimationData = json;
@@ -59,12 +59,19 @@ async function loadAssets() {
 
 		// manual orientation bc i suck at blender
 		chair.rotation.x = THREE.MathUtils.degToRad(90);
-		// chair.position.set(0, 0, 0);
-		// brush.position.set(-1.766, 0.918, 1.118);
-		brush.visible = false;
+		// chair.position.set(-0.046, -1, -1.995); // chair animation start frame location
+		// brush.position.set(-0.046, -1, -1.8); // a little offset because (0,0,0) isn't middle of chair animation
+		brush.rotation.x = THREE.MathUtils.degToRad(90);
+
+		// updates values scene uses? 
+		chair.updateMatrixWorld(true);
+		brush.updateMatrixWorld(true);
+
 
 		scene.add(chair, brush);
-		console.log('scene', scene);
+		console.log('scene', AnimationData.stool.length, scene);
+		brush.visible = true;
+
 
 	}catch (err) {
 		console.error('Error loading glb model:', err);
@@ -75,20 +82,26 @@ async function loadAssets() {
 // --- create model particles for transition --- \\
 function createParticles() {
 	// config
-	const counts = { white: 4000, black: 1500 };
+	const counts = { white: 6000, black: 2000 };
+
+	// initial setup of colors of points
 	const materials = {
-		white: new THREE.PointsMaterial({ size: .0125, sizeAttenuation: true, color: 0x999999 }),
+		white: new THREE.PointsMaterial({ size: .0125, sizeAttenuation: true, color: 0xE4E4E4 }),
 		black: new THREE.PointsMaterial({ size: .0125, sizeAttenuation: true, color: 0x111111 })
 	};
 
+	// models both have two different sections. Grab them here
 	const chairWhiteMesh = chair.children[0].children[0];
 	const chairBlackMesh = chair.children[0].children[1];
-	const brushMesh = brush.children[0];
+	const brushBodyMesh = brush.children[0];
+	const brushTipMesh = brush.children[1];
 
+	// setup for grabbing random points on mesh
 	const sampler = {
 		white: new MeshSurfaceSampler(chairWhiteMesh).build(),
 		black: new MeshSurfaceSampler(chairBlackMesh).build(),
-		brush: new MeshSurfaceSampler(brushMesh).build()
+		brushBody: new MeshSurfaceSampler(brushBodyMesh).build(),
+		brushTip: new MeshSurfaceSampler(brushTipMesh).build()
 	};
 
 	// sample N points from a surface sampler
@@ -117,14 +130,22 @@ function createParticles() {
 		return new THREE.Points(geometry, material);
 	}
 
-	const pointsW = makePointCloud(counts.white, materials.white, sampler.white, sampler.brush);
-	const pointsB = makePointCloud(counts.black, materials.black, sampler.black, sampler.brush);
+	const pointsW = makePointCloud(counts.white, materials.white, sampler.white, sampler.brushBody);
+	const pointsB = makePointCloud(counts.black, materials.black, sampler.black, sampler.brushTip);
+
+	// set transparency here (for later animations)
+	pointsW.material.transparent = true;
+	pointsW.material.opacity = 0;
+	pointsB.material.transparent = true;
+	pointsB.material.opacity = 0;
+	chairWhiteMesh.material.transparent = true;
+	chairBlackMesh.material.transparent = true;
 
 	// group & position them together
 	points = new THREE.Group();
 	points.add(pointsW, pointsB);
 	points.rotation.x = THREE.MathUtils.degToRad(90);
-	points.position.set(-1.7, 0.922, 0.918);
+	points.position.set(-0.046, -1, -1.995); // match end frame of chair animation
 	points.visible = false;
 
 	scene.add(points);
@@ -141,7 +162,7 @@ async function init() {
 }
 
 
-let transitionStarted = true;
+let transitionStarted = false;
 // --- --- \\
 function animate(time) {
 	requestAnimationFrame(animate);
@@ -149,22 +170,24 @@ function animate(time) {
 	Tween.update(time);
 
 	switch(state) {
-		case 'chair': updateChair(); break;
+		case 'chair': {
+			updateChair();
+			break;
+		}
 		case 'transition': {
-			if(transitionStarted) {
-				console.log('rIRANIRANIRANIRAN&&&&&&&&&&&&&&&&&&&&&&&&&&&&an')
-				transitionStarted = false;
+			if(!transitionStarted) {
+				transitionStarted = true;
 				startTweenTransition();
 			}
 			break;
 		}
-		case 'brush': return;
+		case 'brush': {
+			return;
+		}
 	}
 
 	renderer.render(scene, camera);
 }
-
-const array = [];
 
 function updateChair() {
 	const path = AnimationData.stool;
@@ -173,61 +196,66 @@ function updateChair() {
 		chair.position.set(x, y, z);
 		camera.lookAt(chair.position);
 
+		// transparency switch of chair + chair points
+		if(frameCount > AnimationData.stool.length - 200) {
+			points.visible = true;
+			points.position.set(x, y, z); // HERE, points aren't visible? 
+
+			chair.children[0].children.map(child => child.material.opacity = Math.max(0, child.material.opacity - .005));
+			points.children.map(child => child.material.opacity = Math.min(1, child.material.opacity + .005));
+			console.log(points.children[0].material.opacity);
+		}
 	} else {
-		frameCount = 1;
+		chair.visible = false;
+		frameCount = 0;
 		state = 'transition';
 	}
 }
 
 function startTweenTransition() {
-	// hide the originals
-	chair.visible = brush.visible = false;
-	points.visible = true;
-
-	// capture initial & target arrays for each point
-	let startW = points.children[0].geometry.attributes.position;
-	let startB = points.children[1].geometry.attributes.position;
+	// capture target arrays + color
 	const targetW = points.children[0].geometry.attributes.targetPosition.array;
 	const targetB = points.children[1].geometry.attributes.targetPosition.array;
+	const targetColor = {
+		"r": 0.04817182422013895,
+		"g": 0.04817182422013895,
+		"b": 0.04817182422013895
+	}
 
 	new Tween.Tween({ t:0 })
-		.to({ t: 1 }, 8000)
+		.to({ t: .05 }, 8000)
 		.easing(Easing.Exponential.Out)
 		.onUpdate(o => {
-		function calcStep(start, targetArr) {
-			for (let i = 0; i < start.array.length; i++) {
-				const endPos = THREE.MathUtils.lerp(start.array[i], targetArr[i], o.t);
-				start.array[i] = endPos;
+		function calcStep(startMesh, targetArr) {
+			let posArr = startMesh.geometry.attributes.position.array;
+			let startColor = startMesh.material.color;
+
+			for (let i = 0; i < posArr.length; i++) {
+				const endPos = THREE.MathUtils.lerp(posArr[i], targetArr[i], o.t);
+				posArr[i] = endPos;
 			}
+			const r = THREE.MathUtils.lerp(startColor.r, targetColor.r, o.t);
+			const g = THREE.MathUtils.lerp(startColor.g, targetColor.g, o.t);
+			const b = THREE.MathUtils.lerp(startColor.b, targetColor.b, o.t);
+			startColor.setRGB(r, g, b);
 
-			// 	// points.position.set(-1.7, 0.922, 0.918);
-			// // const {x, y, z} = {x: -1.7, y: 0.922, z:.918};
-			// const {x, y, z} = points.position;
-			// points.position.set(x + .01, y, z + o.t/2);
-			// // console.log(x + o.t/10)
-			// camera.lookAt(points.position);
-			// start.needsUpdate = true;
-
-			return start;
+			startMesh.geometry.attributes.position.needsUpdate = true;
 		}
-
-		console.log('cam', camera.position);
-		console.log('brush', brush.position);
 		
-		startW = calcStep(startW, targetW);
-		startB = calcStep(startB, targetB);
+		calcStep(points.children[0], targetW);
+		calcStep(points.children[1], targetB);
 
-		// camera moves in a circle, don't have to center on brush because on same y level. Just tell it to look at it
-		const circleRadius = 2;
-		const circleSpeed  = 2;  
-
-		const theta = o.t * Math.PI;
-		camera.position.x = brush.position.x + circleRadius * Math.cos(theta);
-		camera.position.z = brush.position.z + circleRadius * Math.sin(theta);
-		camera.lookAt(brush.position);
+		frameCount++;
 
 		}).onComplete(() => {
 			state = 'brush';
 		})
 		.start();
 }
+
+
+// /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \
+// --disable-web-security \
+// --user-data-dir="/tmp/chrome-dev-disable-cors" \
+// --allow-file-access-from-files \                                                --enable-precise-memoryinfo \
+// file:///Users/samuel/code/Elijah/Pi-code/Animation/camera.html
